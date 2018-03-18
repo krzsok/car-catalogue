4591bd2c84f327b19c99919cbda43d4d1aeae792
krzsok <krzsokmail@gmail.com>
2018-03-18 22:34:31 +0100

    i
    i

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+﻿var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+
+var con = mysql.createConnection({
+	host: 'localhost', /* host: '172.30.24.12'  */
+	user: 'xxxx',
+	password: 'xxxx',
+	database: 'xxxx'
+});
+con.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var sql = 'SELECT * FROM listasam';
+  con.query(sql, function(err,rows){
+        res.render('index', { title: 'Katalog samochodów', dane: rows });
+   });
+});
+
+module.exports = router;
