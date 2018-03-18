fe2df39c4809992719b04a3b8cff61202e9725cc
krzsok <krzsokmail@gmail.com>
2018-03-18 23:25:27 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 var router = express.Router();
 
 var con = mysql.createConnection({
-	host: 'localhost', /* host: '172.30.24.12' uczelniany */
-	user: 'sokol_1089292',
-	password: '742482439',
-	database: 'SOKOL'
+	host: 'localhost', /* '172.30.24.12' uczelniany */
+	user: 'xxxx',
+	password: 'xxxx',
+	database: 'xxxx'
 });
 con.connect();
 
